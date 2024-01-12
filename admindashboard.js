255dad575c00b128b1287392cdfacee98c7d616c
Swapnil Bamble <swapnilbamble4@gmail.com>
2024-01-12 20:10:04 +0530

    Update admindashboard.js

--- admindashboard.js
+++ admindashboard.js
@@ -3,6 +3,7 @@ showAdminDashboard();
 
 function showAdminDashboard()
 {
+    
     let candidates = localStorage.getItem("allcandidates");
     if(candidates == null)
     {
@@ -11,12 +12,31 @@ function showAdminDashboard()
     else{
         candidatesObj = JSON.parse(candidates);
     }
-    
-    
-    document.getElementById("c1votes").innerHTML = candidatesObj[0];
-    document.getElementById("c2votes").innerHTML = candidatesObj[1];
-    document.getElementById("c3votes").innerHTML = candidatesObj[2];
-    document.getElementById("c4votes").innerHTML = candidatesObj[3];
+  
+   var c1totalvotes = 0;
+   var c2totalvotes = 0;
+   var c3totalvotes = 0;
+   var c4totalvotes= 0;
+
+    if(candidatesObj.length === 0 )
+    {
+        c1totalvotes = 0;
+        c2totalvotes = 0;
+        c3totalvotes = 0;
+        c4totalvotes= 0;
+    }
+    else{
+          
+     c1totalvotes =candidatesObj[0];
+     c2totalvotes =candidatesObj[1];
+     c3totalvotes = candidatesObj[2];
+     c4totalvotes = candidatesObj[3];
+    }
+    document.getElementById("c1votes").innerHTML = c1totalvotes;
+    document.getElementById("c2votes").innerHTML = c2totalvotes;
+    document.getElementById("c3votes").innerHTML = c3totalvotes;
+    document.getElementById("c4votes").innerHTML = c4totalvotes;
+
 
 
 
