8b446aaab386d7e87379baa50355b3ff99c2759e
Swapnil Bamble <swapnilbamble4@gmail.com>
2024-01-12 18:41:14 +0530

    Update admindashboard.js

--- admindashboard.js
+++ admindashboard.js
@@ -12,17 +12,20 @@ function showAdminDashboard()
         candidatesObj = JSON.parse(candidates);
     }
     
-    let c1totalvotes =candidatesObj[0];
-    let c2totalvotes =candidatesObj[1];
-    let c3totalvotes = candidatesObj[2];
-    let c4totalvotes = candidatesObj[3];
-
+    
     if( candidatesObj == undefined || candidatesObj == null )
     {
         c1totalvotes = 0;
         c2totalvotes = 0;
         c3totalvotes = 0;
         c4totalvotes= 0;
+    }
+    else{
+          
+    let c1totalvotes =candidatesObj[0];
+    let c2totalvotes =candidatesObj[1];
+    let c3totalvotes = candidatesObj[2];
+    let c4totalvotes = candidatesObj[3];
     }
  
     
